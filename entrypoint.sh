5e9c77f9df63e4c5770514b4db7869a726be85a8
James.Hester <jxh@ansto.gov.au>
2020-06-12 17:23:16 +1000

    Initial files

---
+++ entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -l
+
+# get latest cifapi
+
+git clone https://github.com/COMCIFS/cif_api cif_api
+cd cif_api
+./configure
+make
+
+gcc -o cif_syncheck -licuio src/examples/syncheck.c src/.libs/libcif.so.1
+
+chmod a+x cif_syncheck
+
+# run the checks
+
+./cif_syncheck $1
+
